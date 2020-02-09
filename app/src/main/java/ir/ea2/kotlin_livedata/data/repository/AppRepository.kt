913721c765878406bbb9fcdde38cba7badc5d6fe
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-09 10:42:55 +0330

    Commit> Add AppRepository (Access To All Method's Of ApiService).

---
+++ app/src/main/java/ir/ea2/kotlin_livedata/data/repository/AppRepository.kt
@@ -0,0 +1,35 @@
+package ir.ea2.kotlin_livedata.data.repository
+
+import ir.ea2.kotlin_livedata.data.remote.RetrofitService
+import ir.ea2.kotlin_livedata.data.remote.model.NoteResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+//In AppRepository Class , We Access To All Method's Of ApiService.
+class AppRepository private constructor(){
+    companion object{
+        //Create Private Object From Own Class.
+        private val singeltonInstance :AppRepository = AppRepository()
+
+        fun getInstance(): AppRepository{
+            //When Writing : AppRepository.getInstance() , We Access All Method's Of This Class.
+            return singeltonInstance
+        }
+    }
+    fun getNotes(){
+        //Method enqueue() : Use For Create Enqueue Of Request.After Any Callback Get Answer, GoTo Next Request.
+        //For Create Request Using Callback interface and Implemented Method's.
+        RetrofitService.apiService.getNote().enqueue(object: Callback<NoteResponse>{
+            //Any Request Have Two State: RequestHasError Or RequestIsOk .
+            //onFailure : RequestHasError And Detail's Available In Throwable Variable.
+            override fun onFailure(call: Call<NoteResponse>, t: Throwable) {
+
+            }
+            //onResponse : RequestIsOk And Detail's Available In Response Variable.
+            override fun onResponse(call: Call<NoteResponse>, response: Response<NoteResponse>) {
+
+            }
+        })
+    }
+}
